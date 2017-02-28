0317a10ae7e822fdcbea6ee06d2deb24ec9c05a6
fausto_fusaro <fausto.fusaro@injenia.it>
2017-02-28 12:25:38 +0100

    added export env variables

--- init/env.sh
+++ init/env.sh
@@ -6,3 +6,10 @@ echo SQL_USERNAME="[your_sql_username]" >> config
 echo SQL_PASSWORD="[your_sql_password]" >> config
 echo SQL_CONNECTION_NAME=$(curl -s "http://metadata/computeMetadata/v1/instance/attributes/sql-connection-name" \
     -H "Metadata-Flavor: Google") >> config
+
+
+# update env variables
+export DESTINATION_BUCKET
+export SQL_USERNAME
+export SQL_PASSWORD
+export SQL_CONNECTION_NAM
